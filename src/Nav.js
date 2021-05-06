48c2eaaf360a990a98b5f26062a394ed471eae52
papillon129245 <marfro245@gmail.com>
2021-05-06 21:55:23 +0000

    ADD: 1st day

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+
+const [show, handleShow] = useState(false);
+
+const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+        handleShow(true);    
+    }   else {
+        handleShow(false)
+    }
+}
+
+useEffect(() => {
+window.addEventListener("scroll", transitionNavBar);
+return () => window.removeEventListener("scroll", transitionNavBar);    
+}, []);
+
+    return (
+        <div className={`nav ${show && `nav__black`}`}>
+            <div className='nav__content'>
+                <img className='nav__logo' src="http://www.freepnglogos.com/uploads/netflix-logo-0.png" alt='' /> 
+                <img className='nav__avatar' src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuybor.uz%2Fborless%2Favtobor%2Fimg%2Fuser-images%2Fuser_no_photo_512x512.png&f=1&nofb=1" alt='' />
+            </div>
+        </div>
+    )
+}
+
+export default Nav
+
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDr-35O4zN_vPTZEJByzeafi4w0Ed49k0A",
+    authDomain: "netflix-build-fda3f.firebaseapp.com",
+    projectId: "netflix-build-fda3f",
+    storageBucket: "netflix-build-fda3f.appspot.com",
+    messagingSenderId: "563877218945",
+    appId: "1:563877218945:web:8ed790f22d13d166c91b80"
+  };
